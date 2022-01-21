c51ed68f0bc5c7b84f8796195c5ad1b1a41453d1
ChanWoo <jungcali94@gmail.com>
2022-01-21 17:07:47 +0900

    백트래킹(추가 작성바람)

---
+++ src/algorithm/BackTracking.java
@@ -0,0 +1,14 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BackTracking {
+
+    public ArrayList<int[]> NQueen(int n) {
+        ArrayList<int[]> backtrack = new ArrayList<>();
+
+
+        return backtrack;
+    }
+}
