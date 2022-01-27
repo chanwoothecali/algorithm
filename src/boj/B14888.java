7a615b59d8b6e1d59ce8d15346382b83d2dab2b1
ChanWoo <jungcali94@gmail.com>
2022-01-27 14:44:32 +0900

    boj 14888 리팩토링 (강사 풀이)

--- src/boj/B14888.java
+++ src/boj/B14888.java
@@ -3,7 +3,7 @@ package boj;
 public class B14888 {
     static int N, max, min;
     static int[] numbers, order;
-    static final int[] operators = new int[4];
+    static final int[] operators = new int[5];
 
     static void input() {
         FastReader scan = new FastReader();
@@ -13,7 +13,7 @@ public class B14888 {
         for (int i = 1; i <= N; i++) {
             numbers[i] = scan.nextInt();
         }
-        for (int i = 0; i < 4; i++) {
+        for (int i = 1; i <= 4; i++) {
             operators[i] = scan.nextInt();
         }
 
@@ -21,17 +21,17 @@ public class B14888 {
         min = Integer.MAX_VALUE;
     }
 
-    static void reqFunc(int k) {
+    static void reqFunc(int k, int value) {
         if(k == N) {
-            int result = calculate();
-            max = Math.max(max, result);
-            min = Math.min(min, result);
+            max = Math.max(max, value);
+            min = Math.min(min, value);
         }else {
-            for (int i = 0; i < 4; i++) {
+            for (int i = 1; i <= 4; i++) {
                 if(operators[i] > 0){
                     order[k] = i;
                     operators[i]--;
-                    reqFunc(k + 1);
+                    int result = calculate(value, i, numbers[k + 1]);
+                    reqFunc(k + 1, result);
                     operators[i]++;
                     order[k] = 0;
                 }
@@ -39,31 +39,31 @@ public class B14888 {
         }
     }
 
-    private static int calculate() {
-        int result = numbers[1];
-        for (int i = 1; i < N; i++) {
-            switch (order[i]) {
-                case 0:
-                    result += numbers[i+1];
-                    break;
-                case 1:
-                    result -= numbers[i+1];
-                    break;
-                case 2:
-                    result *= numbers[i+1];
-                    break;
-                case 3:
-                    result /= numbers[i+1];
-                    break;
-            }
+    // 피연산자 2개와 연산자가 주어졌을 때 연산하는 함수
+    private static int calculate(int currVal, int operator, int nextVal) {
+        switch (operator) {
+            case 1:
+                currVal += nextVal;
+                break;
+            case 2:
+                currVal -= nextVal;
+                break;
+            case 3:
+                currVal *= nextVal;
+                break;
+            case 4:
+                currVal /= nextVal;
+                break;
+            default:
+                break;
         }
-        return result;
+        return currVal;
     }
 
     public static void main(String[] args) {
         input();
 
-        reqFunc(1);
+        reqFunc(1, numbers[1]);
         System.out.println(max);
         System.out.println(min);
     }
