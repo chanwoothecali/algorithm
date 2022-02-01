d59389cb41cb304486d23dc2709dca292e70aba5
jungcali <jungcali94@gmail.com>
2022-02-01 16:23:12 +0900

    boj 1015 풀이O(NlogN)

--- src/boj/B1015.java
+++ src/boj/B1015.java
@@ -5,50 +5,49 @@ import java.util.Arrays;
 public class B1015 {
 
     static FastReader scan = new FastReader();
-    static StringBuilder sb = new StringBuilder();
-    static int[] P, B, A;
+    static Element[] B;
+    static int[] P;
     static int N;
+
+    static class Element implements Comparable<Element>{
+        int idx;
+        int num;
+
+        public Element(int idx, int num) {
+            this.idx = idx;
+            this.num = num;
+        }
+
+        @Override
+        public int compareTo(Element o) {
+            if(this.num == o.num) return this.idx - o.idx;
+            return this.num - o.num;
+        }
+    }
 
     static void input() {
         N = scan.nextInt();
+        B = new Element[N];
         P = new int[N];
-        A = new int[N];
-        B = new int[N];
         for (int i = 0; i < N; i++) {
-            A[i] = scan.nextInt();
+            B[i] = new Element(i, scan.nextInt());
         }
     }
 
-    static void reqFunc() {
-        B = sort(A);
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if(A[i] == B[j]) {
-                    P[i] = j;
-                    B[j] = -1;
-                    break;
-                }
-            }
+    static void func() {
+        Arrays.sort(B);
 
+        for (int i = 0; i < N; i++) {
+            P[B[i].idx] = i;
         }
-    }
 
-    static int[] sort(int[] a) {
-        int[] b = a.clone();
-        Arrays.sort(b);
-        return b;
-    }
-
-    static void print() {
-        for (int i = 0; i < N; i++) {
-            System.out.print(P[i] + " ");
+        for (int i : P) {
+            System.out.print(i + " ");
         }
     }
 
     public static void main(String[] args) {
         input();
-
-        reqFunc();
-        print();
+        func();
     }
 }
