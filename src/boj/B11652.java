2eeb6c0ae9d60b30d9bef02817caeefd8c1c6a1d
ChanWoo <jungcali94@gmail.com>
2022-02-03 16:30:30 +0900

    주석 추가

--- src/boj/B11652.java
+++ src/boj/B11652.java
@@ -20,6 +20,7 @@ public class B11652 {
         Arrays.sort(numbers);
         int currCount = 1;
         int maxCount = 1;
+        // maxValue 0으로 시작시 같은 번호로만 이루어진 카드의 최대값 판별 불가
         long maxValue = numbers[0];
         for (int i = 1; i < N; i++) {
             if (numbers[i - 1] == numbers[i]) {
@@ -27,6 +28,7 @@ public class B11652 {
             } else {
                 currCount = 1;
             }
+            // else구문에 아래 if문을 넣을시 첫value가 maxValue인 경우 캐치 불가
             if (currCount > maxCount) {
                 maxCount = currCount;
                 maxValue = numbers[i - 1];
