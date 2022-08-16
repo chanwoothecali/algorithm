f00677969d55c08e434e762605270108f102d9d9
JungCali <jungcali94@gmail.com>
2022-08-16 17:30:45 +0900

    b2252 리팩토링

--- src/boj/topologicalsort/B2252.java
+++ src/boj/topologicalsort/B2252.java
@@ -2,10 +2,7 @@ package boj.topologicalsort;
 
 import boj.FastReader;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class B2252 {
 
@@ -14,10 +11,8 @@ public class B2252 {
     private int N;
     // 1 ~ 100,000
     private int M;
-    ArrayList<Integer>[] children;
-    ArrayList<Integer>[] parents;
-    Queue<Integer> noInDegrees = new LinkedList<>();
-    StringBuilder sb = new StringBuilder();
+    private int[] indeg;
+    ArrayList<Integer>[] adj;
 
     public void main() {
         input();
@@ -28,35 +23,33 @@ public class B2252 {
         StringTokenizer nmToken = scan.spaceToken();
         N = Integer.parseInt(nmToken.nextToken());
         M = Integer.parseInt(nmToken.nextToken());
-        children = new ArrayList[N + 1];
-        parents = new ArrayList[N + 1];
+        adj = new ArrayList[N + 1];
+        indeg = new int[N + 1];
         for (int i = 1; i <= N; i++) {
-            children[i] = new ArrayList<>();
-            parents[i] = new ArrayList<>();
+            adj[i] = new ArrayList<>();
         }
         for (int i = 0; i < M; i++) {
             StringTokenizer st = scan.spaceToken();
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            children[x].add(y);
-            parents[y].add(x);
-        }
-        for (int i = 1; i <= N; i++) {
-            if (parents[i].isEmpty()) {
-                noInDegrees.offer(i);
-            }
+            adj[x].add(y);
+            indeg[y]++;
         }
     }
 
     private void solve() {
+        StringBuilder sb = new StringBuilder();
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            if (indeg[i] == 0) queue.offer(i);
+        }
 
-        while (!noInDegrees.isEmpty()) {
-            int x = noInDegrees.poll();
+        while (!queue.isEmpty()) {
+            int x = queue.poll();
             sb.append(x).append(' ');
-            ArrayList<Integer> child = children[x];
-            for (Integer index : child) {
-                parents[index].remove((Integer) x);
-                if (parents[index].isEmpty()) noInDegrees.offer(index);
+            for (Integer nextAdj : adj[x]) {
+                indeg[nextAdj]--;
+                if (indeg[nextAdj] == 0) queue.offer(nextAdj);
             }
         }
 
