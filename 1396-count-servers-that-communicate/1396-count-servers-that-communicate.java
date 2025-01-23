076f0d1fe40e001b79c64696d46ab678c17c04d4
ChanWoo Jung <jungcali94@gmail.com>
2025-01-23 11:36:03 +0900

    Time: 2 ms (100%), Space: 53 MB (9.06%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.java
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.java
@@ -1,17 +1,14 @@
 class Solution {
     public int countServers(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
+        final int m = grid.length;
+        final int n = grid[0].length;
 
         int[] rows = new int[m];
         int[] lines = new int[n];
 
-        int[][] visited = new int[m][n];
-        Queue<int[]> queue = new LinkedList<>();
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
-                    queue.offer(new int[]{i, j});
                     rows[i]++;
                     lines[j]++;
                 }
@@ -19,13 +16,11 @@ class Solution {
         }
 
         int count = 0;
-        while (!queue.isEmpty()) {
-            int[] column = queue.poll();
-            int row = column[0];
-            int line = column[1];
-
-            if (rows[row] > 1 || lines[line] > 1) {
-                count++;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1 && (rows[i] > 1 || lines[j] > 1)) {
+                    count++;
+                }
             }
         }
 
