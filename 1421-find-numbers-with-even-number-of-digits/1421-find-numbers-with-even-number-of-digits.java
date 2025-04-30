20e018c9f15fc39d2b5fb32c815bd2aef2643b28
ChanWoo Jung <jungcali94@gmail.com>
2025-04-30 09:49:22 +0900

    Time: 1 ms (98.76%), Space: 42.6 MB (95.2%) - LeetHub

--- 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
@@ -2,11 +2,21 @@ class Solution {
     public int findNumbers(int[] nums) {
         int evenDigitsCount = 0;
         for (int num : nums) {
-            if (String.valueOf(num).length() % 2 == 0) {
+            if (isEvenDigitsCount(num)) {
                 evenDigitsCount++;
             }
         }
 
         return evenDigitsCount;
+    }
+
+    private boolean isEvenDigitsCount(int num) {
+        int digits = 1;
+        while (num / 10 > 0) {
+            digits++;
+            num /= 10;
+        }
+
+        return digits % 2 == 0 ? true : false;
     }
 }
